02d91a957a177e60576670f831f79b1877300f41
Rik Meijer <rikmeijer@gmail.com>
2016-09-08 13:14:54 +0200

    array of fields

--- source/sql/select.d
+++ source/sql/select.d
@@ -1,19 +1,23 @@
 module sql.select;
 
+import std.array;
+
 interface Source {
 	public string appendIdentifier(string query);
 }
 
 class Select {
 	
+	private string[] fields;
 	private Source source;
 	
 	public this(Source source) {
 		this.source = source;
+		this.fields ~= "*";
 	}
 	
 	public string generate() {
-		return this.source.appendIdentifier("SELECT * FROM ");
+		return this.source.appendIdentifier("SELECT " ~ join(this.fields) ~ " FROM ");
 	}
 	
 } unittest {
