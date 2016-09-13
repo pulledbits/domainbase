4d876a49e27e57cb4307e4b17e0751bb3088d0c0
Rik Meijer <rikmeijer@gmail.com>
2016-09-13 16:47:00 +0200

    add field alias

--- source/sql/select.d
+++ source/sql/select.d
@@ -12,16 +12,20 @@ class Select {
 	}
 	
 	public string generate() {
-		if (this.tableIdentifier is null) {
+		string source = "";
+		if (this.tableIdentifier !is null) {
+			source = " FROM " ~ this.tableIdentifier;
+		} else if (this.fields.length == 0) {
 			return "SELECT NULL";
 		}
+		
 		string fieldsSQL;
 		if (this.fields.length == 0) {
 			fieldsSQL = "*";
 		} else {
 			fieldsSQL = join(this.fields, ',');
 		}
-		return "SELECT " ~ fieldsSQL ~ " FROM " ~ this.tableIdentifier;
+		return "SELECT " ~ fieldsSQL ~ source;
 	}
 	
 	public void from(string tableIdentifier)
@@ -34,14 +38,26 @@ class Select {
 		this.fields ~= '"' ~ text ~ '"';
 	}
 	
+	public void select(string text, string as) 
+	{
+		this.fields ~= '"' ~ text ~ "\" AS " ~ as;
+	}
+	
 } unittest {
 	Select query = new Select();
 	assert(query.generate() == "SELECT NULL");
 	
+	
+	query = new Select();
 	query.from("mytable");
 	assert(query.generate() == "SELECT * FROM mytable");
 	
+	query = new Select();
 	query.select("foobar");
-	assert(query.generate() == "SELECT \"foobar\" FROM mytable");
+	assert(query.generate() == "SELECT \"foobar\"");
+	
+	query = new Select();
+	query.select("foo", "bar");
+	assert(query.generate() == "SELECT \"foo\" AS bar");
 	
 }
