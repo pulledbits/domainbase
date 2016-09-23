1c6a1f5ebeff0b3c73981ed39c4363bbe2caf8a6
Rik Meijer <rikmeijer@gmail.com>
2016-09-23 09:00:13 +0200

    make queries starting from table

--- source/sql/table.d
+++ source/sql/table.d
@@ -1,9 +1,25 @@
+import select;
+import column;
+
 class Table {
 	
 	private string identifier;
 	
 	public this(string identifier) {
 		this.identifier = identifier;
+	}
+	
+	public Select select(string column)
+	{
+		Select query = new Select();
+		query.select(new Column(this.identifier, column));
+		return query;
+	}
+	unittest {
+		Table table = new Table("mytable");
+		Select query = table.select("mycolumn");
+		
+		assert(query.generate() == "SELECT mytable.mycolumn");
 	}
 	
 } unittest {
