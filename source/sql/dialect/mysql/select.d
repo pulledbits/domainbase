8acc40353f38c3af00ea400f57ab3b0fd2758b45
Rik Meijer <rikmeijer@gmail.com>
2017-05-04 15:59:04 +0200

    beautify code

--- source/sql/dialect/mysql/select.d
+++ source/sql/dialect/mysql/select.d
@@ -5,68 +5,88 @@ import sql.field;
 
 import std.array;
 
-class Select : sql.select.Select {
-	private string[] fields;
-	private Table from;
+class Select : sql.select.Select
+{
+    private string[] fields;
+    private Table from;
 
-	public this() {
-	}
-	unittest {
-		Select query = new Select();
-		assert(query.generate() == "SELECT NULL");
-	}
+    public this()
+    {
+    }
 
-	public this(Table from) {
-		this.from = from;
-	}
-	unittest {
-		Select query = new Select(new Table("mytable"));
-		assert(query.generate() == "SELECT NULL FROM `mytable`");
-	}
+    unittest
+    {
+        Select query = new Select();
+        assert(query.generate() == "SELECT NULL");
+    }
 
-	public string generate() {
-		string query = "SELECT";
-		string source = "";
+    public this(Table from)
+    {
+        this.from = from;
+    }
 
-		if (this.from !is null) {
-			source = " FROM `mytable`";
-		}
+    unittest
+    {
+        Select query = new Select(new Table("mytable"));
+        assert(query.generate() == "SELECT NULL FROM `mytable`");
+    }
 
-		string fieldsSQL = " ";
-		if (this.fields.length == 0) {
-			fieldsSQL ~= "NULL";
-		} else {
-			fieldsSQL ~= join(this.fields, ',');
-		}
+    public string generate()
+    {
+        string query = "SELECT";
+        string source = "";
 
-		return query ~ fieldsSQL ~ source;
-	}
+        if (this.from !is null)
+        {
+            source = " FROM `mytable`";
+        }
 
-	public void select(Field field)
-	{
-		this.fields ~= field.generate();
-	}
-	unittest {
-		Select query = new Select();
-		query.select(new class Field {
-			public string generate() {
-				return "FooBar";
-			}
-		});
-		assert(query.generate() == "SELECT FooBar");
-	}
+        string fieldsSQL = " ";
+        if (this.fields.length == 0)
+        {
+            fieldsSQL ~= "NULL";
+        }
+        else
+        {
+            fieldsSQL ~= join(this.fields, ',');
+        }
 
-	public void select(Field field, string as)
-	{
-		this.fields ~= field.generate() ~ " AS " ~ as;
-	}
-	unittest {
-		Select query = new Select();
-		query.select(new class Field {
-			public string generate() {
-				return "FooBar";
-			}
-		}, "bar");
-		assert(query.generate() == "SELECT FooBar AS bar");
-	}
-}
+        return query ~ fieldsSQL ~ source;
+    }
+
+    public void select(Field field)
+    {
+        this.fields ~= field.generate();
+    }
+
+    unittest
+    {
+        Select query = new Select();
+        query.select(new class Field
+        {
+            public string generate()
+            {
+                return "FooBar";}
+            }
+);
+            assert(query.generate() == "SELECT FooBar");
+        }
+
+        public void select(Field field, string as)
+        {
+            this.fields ~= field.generate() ~ " AS " ~ as;
+        }
+
+        unittest
+        {
+            Select query = new Select();
+            query.select(new class Field
+            {
+                public string generate()
+                {
+                    return "FooBar";}
+                }
+, "bar");
+                assert(query.generate() == "SELECT FooBar AS bar");
+            }
+        }
