785f7314ce94723006b480245a16e8a539d1324e
Rik Meijer <rikmeijer@gmail.com>
2017-10-23 16:06:16 +0200

    mysql-d no longer available

--- source/app.d
+++ source/app.d
@@ -1,14 +1,6 @@
 import std.stdio;
-import mysql.d;
 
 void main()
 {
     writeln("Hello World.");
-
-    auto mysql   = new Mysql("localhost", 3306, "teach", "teach", "teach");
-    auto ratings = mysql.query("SELECT waarde FROM rating ORDER BY updated_at DESC");
-    foreach (rating; ratings)
-    {
-        writefln("Rating: %s", rating["waarde"]);
-    }
 }
